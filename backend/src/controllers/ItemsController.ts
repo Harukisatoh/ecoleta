026c8ef2eb1e8058e57bb1488550f10d6d582eae
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2020-06-05 21:34:58 -0300

    fix: item prop renamed

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
             return {
                 id: item.id,
                 name: item.name,
-                image: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://localhost:3333/uploads/${item.image}`
             }
         });
 
