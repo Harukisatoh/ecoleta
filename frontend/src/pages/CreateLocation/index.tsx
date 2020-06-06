695de5e1547b32ef21195df088199d8dc0a29330
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2020-06-05 21:28:13 -0300

    feat: implementing home page

---
+++ frontend/src/pages/CreateLocation/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+
+
+const CreateLocation = () => {
+    return (
+        <h1>Create Location</h1>
+    );
+};
+
+export default CreateLocation;
