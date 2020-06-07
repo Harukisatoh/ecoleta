2e0676d2501da539c375cd67ab71bfd94498f2ce
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2020-06-06 21:16:50 -0300

    fix: created a safe area view outside all content

--- mobile/src/pages/Locations/styles.tsx
+++ mobile/src/pages/Locations/styles.tsx
@@ -2,6 +2,10 @@ import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
+
     container: {
         flex: 1,
         paddingHorizontal: 32,
